640c7ab4d2d00753c34ade9b92e0dad07156c4fc
krishnavamsi-jasti <jkv.jasti@gmail.com>
2026-03-13 18:46:12 +0530

    This is the varibles type

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
     region = "us-east-1"
 }
 resource "aws_instance" "test" {
-    instance_type = "t2.micro"  
+    instance_type = var.instance_type  
     ami = "ami-02dfbd4ff395f2a1b"
+}
+
+
+resource "aws_iam_user" "test-user" {
+    name = var.iam-user
 }
